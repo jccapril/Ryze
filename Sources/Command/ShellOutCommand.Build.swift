131f8b4a3dc00ad2a424b3a985007efc6972d1af
JiangChencheng <chencheng.jiang@v.show>
2024-12-05 18:29:03 +0800

    configuration添加默认值

--- Sources/Command/ShellOutCommand.Build.swift
+++ Sources/Command/ShellOutCommand.Build.swift
@@ -16,17 +16,17 @@ enum Configuration: String, Decodable {
 extension ShellOutCommand {
         
     /// Clean
-    static func xcodebuildClean(workspace: String, scheme: String, configuration: Configuration) -> ShellOutCommand {
+    static func xcodebuildClean(workspace: String, scheme: String, configuration: Configuration = .debug) -> ShellOutCommand {
         return ShellOutCommand(string: "xcodebuild clean -workspace \(workspace) -scheme \(scheme) -configuration \(configuration.rawValue) -quiet")
     }
     
     /// Archive
-    static func xcodebuildArchive(workspace: String, scheme: String, configuration: Configuration, archivePath: String) -> ShellOutCommand {
+    static func xcodebuildArchive(workspace: String, scheme: String, configuration: Configuration = .debug, archivePath: String) -> ShellOutCommand {
         return ShellOutCommand(string: "xcodebuild archive -workspace \(workspace) -scheme \(scheme) -configuration \(configuration.rawValue) -archivePath \(archivePath) -quiet")
     }
     
     /// ExportArchive
-    static func xcodebuildExportArchive(archivePath: String, configuration: Configuration, exportPath: String, exportOptionsPlist: String) -> ShellOutCommand {
+    static func xcodebuildExportArchive(archivePath: String, configuration: Configuration = .debug, exportPath: String, exportOptionsPlist: String) -> ShellOutCommand {
         return ShellOutCommand(string: "xcodebuild -exportArchive -archivePath \(archivePath) -configuration \(configuration.rawValue) -exportPath \(exportPath) -exportOptionsPlist \(exportOptionsPlist) -quiet")
     }
     
